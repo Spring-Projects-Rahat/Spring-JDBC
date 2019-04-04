bd8d56ff29c536641b9fb740f1c3331c2a871ee6
Rahat Ali <rahatiu786@gmail.com>
2019-04-04 23:05:09 +0530

    Initial Commit

---
+++ src/main/java/com/java/spring/jdbc/config/SpringJdbcConfig.java
@@ -0,0 +1,23 @@
+package com.java.spring.jdbc.config;
+
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+@Configuration
+@ComponentScan("com.java.spring.jdbc")
+public class SpringJdbcConfig {
+    @Bean
+    public DataSource mysqlDataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql://localhost:3306/testdb");
+        dataSource.setUsername("root");
+        dataSource.setPassword("123456");
+ 
+        return dataSource;
+    }
+}
